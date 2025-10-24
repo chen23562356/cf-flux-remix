4912aad448a5c95009a4e489b6abd0204666aa44
chen23562356 <chongchihin0106@gmail.com>
2025-10-24 23:25:42 +0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -41,7 +41,7 @@ export default function Index() {
                 to="/generate-image"
                 className="block w-full text-center px-6 py-3 text-lg font-semibold text-white bg-gradient-to-r from-indigo-500 to-indigo-700 rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-400"
               >
-                白嫖 CF 的 Flux 生成图片
+                1
               </Link>
             </li>
             <li>
@@ -49,7 +49,7 @@ export default function Index() {
                 to="https://github.com/aigem/cf-flux-remix"
                 className="block w-full text-center px-6 py-3 text-lg font-semibold text-white bg-gradient-to-r from-indigo-500 to-indigo-700 rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-400"
               >
-                访问 Github
+                2
               </Link>
             </li>
             <li>
@@ -57,7 +57,7 @@ export default function Index() {
                 to="https://github.com/aigem/CFr2-webdav"
                 className="block w-full text-center px-6 py-3 text-lg font-semibold text-white bg-gradient-to-r from-indigo-500 to-indigo-700 rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-400"
               >
-                Workers+R2搭建个人免费webdav网盘
+                3
               </Link>
             </li>
             <li>
@@ -65,7 +65,7 @@ export default function Index() {
                 to="https://github.com/aigem/CFr2-webdav"
                 className="block w-full text-center px-6 py-3 text-lg font-semibold text-white bg-gradient-to-r from-indigo-500 to-indigo-700 rounded-xl transition transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-indigo-400"
               >
-                自行搭建：全平台视频下载助手
+                4
               </Link>
             </li>
             {/* 可以在这里添加更多的导航项 */}
